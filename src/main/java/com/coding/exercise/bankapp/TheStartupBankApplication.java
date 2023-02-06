5c595ab6af687556a3b5d3122bfb2cadb03c06f8
james.philip <james.philip@dae.mn>
2023-02-06 12:30:08 +0000

    Adding transfer functionality

--- src/main/java/com/coding/exercise/bankapp/TheStartupBankApplication.java
+++ src/main/java/com/coding/exercise/bankapp/TheStartupBankApplication.java
@@ -2,7 +2,6 @@ package com.coding.exercise.bankapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class TheStartupBankApplication {
 	public static void main(String[] args) {
